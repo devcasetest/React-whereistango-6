07172c6384e837aa84d0b2418a78286603da9b74
whereistango <50135425+whereistango@users.noreply.github.com>
2019-06-21 12:41:41 +0200

    initial commit

---
+++ Calculator/src/test/java/de/sulzer/presentation/CalculatorTest.java
@@ -0,0 +1,32 @@
+package de.sulzer.presentation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void calculateOperationPlus() {
+		Assert.fail("Not implemented test");
+	}
+
+	@Test
+	public void calculateOperationMinus() {
+		Assert.fail("Not implemented test");
+	}
+
+	@Test
+	public void calculateOperationMultiplicate() {
+		Assert.fail("Not implemented test");
+	}
+
+	@Test
+	public void calculateOperationDiv() {
+		Assert.fail("Not implemented test");
+	}
+
+	@Test
+	public void calculateOperationNoValidOperation() {
+		Assert.fail("Not implemented test");
+	}
+}
