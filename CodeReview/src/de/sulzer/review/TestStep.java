1f33e60615b1756c744d332e2c0cc48c88270178
whereistango <50135425+whereistango@users.noreply.github.com>
2019-06-21 12:41:50 +0200

    initial commit

---
+++ CodeReview/src/de/sulzer/review/TestStep.java
@@ -0,0 +1,17 @@
+package de.sulzer.review;
+
+/**
+ * only for demonstration.
+ */
+public class TestStep {
+
+	private HTTPResponse response;
+
+	public HTTPResponse getHttpResponse() {
+		return response;
+	}
+
+	public void setResponse(HTTPResponse response) {
+		this.response = response;
+	}
+}
