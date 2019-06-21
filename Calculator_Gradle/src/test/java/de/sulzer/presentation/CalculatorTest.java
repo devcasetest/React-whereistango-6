56706a81982bcf3189876dfae8bcca645a6f86f0
whereistango <50135425+whereistango@users.noreply.github.com>
2019-06-21 12:41:44 +0200

    initial commit

---
+++ Calculator_Gradle/src/test/java/de/sulzer/presentation/CalculatorTest.java
@@ -0,0 +1,34 @@
+package de.sulzer.presentation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+	@Test
+	public void calculateOperationPlus() {
+		Assert.fail("Not implemented test");
+	}
+
+	@Test
+	public void calculateOperationMinus() {
+		Assert.fail("Not implemented test");
+	}
+
+	@Test
+	public void calculateOperationMultiplicate() {
+		Assert.fail("Not implemented test");
+	}
+
+	@Test
+	public void calculateOperationDiv() {
+		Assert.fail("Not implemented test");
+	}
+
+	@Test
+	public void calculateOperationNoValidOperation() {
+		Assert.fail("Not implemented test");
+	}
+}
